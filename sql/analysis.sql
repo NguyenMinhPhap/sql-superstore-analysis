15efc7d3c06bdc13e2af5b42088fb0345cb4d34b
NguyenMinhPhap <nguyenminhphap8322@gmail.com>
2026-03-11 14:00:24 +0700

    Add queries for top products and customers by sales

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -21,3 +21,20 @@ SUM(Profit) AS total_profit
 FROM Superstore
 GROUP BY Category
 ORDER BY total_profit DESC;
+-- Top product by sales in each category
+SELECT *FROM(SELECT Category,
+        Product_Name,
+        SUM(sales) as total_sales
+         RANK() over(PARTITION  by Category ORDER by total_sales DESC ) as rank_in_category 
+  FROM superstore
+  GROUP BY Category, Product_Name) as ranked_products
+WHERE rank_in_category =1
+
+  
+-- Top 5 customers in each region
+SELECT* FROM ( SELECT region, customer_name,sum(sales) 
+RANK() OVER(PARTITION BY region ORDER BY sum(salels) DESC) AS rank_num
+  FROM superstore
+  GROUP BY region, customer_name) as ranked
+WHERE rank_num <= 5;
+  
